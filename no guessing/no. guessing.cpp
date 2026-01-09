284e467c21189ca9918af7e81e4da86879f20f4f
Gaurav <kalitagaurav47@gmail.com>
2026-01-10 01:26:16 +0530

    ng

--- no guessing/no. guessing.cpp
+++ no guessing/no. guessing.cpp
@@ -1,44 +1,57 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 
 using namespace std;
 
-int main() {
+int main()
+{
     char playAgain;
+    string playerName;
 
-    cout << "WELCOME TO OUR GAME\n";
+    cout << "Enter your name: ";
+    getline(cin, playerName);
+
+    cout << "\nWELCOME TO OUR GAME, " << playerName << "!\n";
     cout << "\n1. Guess the number between 1 and 100";
 
-    do {
+    do
+    {
         srand(time(0));
         int secretNumber = rand() % 100 + 1;
         int guesses[100];
         int guess;
         int attempts = 0;
 
-        cout << "\nLet's play!\n\n";
+        cout << "\n\nLet's play!\n";
         cout << "I have selected a number between 1 and 100.\n";
 
-        do {
+        do
+        {
             cout << "Enter your guess: ";
             cin >> guess;
 
             guesses[attempts] = guess;
             attempts++;
 
-            if (guess > secretNumber) {
+            if (guess > secretNumber)
+            {
                 cout << "Too high! Try again.\n";
-            } 
-            else if (guess < secretNumber) {
+            }
+            else if (guess < secretNumber)
+            {
                 cout << "Too low! Try again.\n";
-            } 
-            else {
-                cout << "\nCongratulations! You guessed the number in "
+            }
+            else
+            {
+                cout << "\nCongratulations, " << playerName
+                     << "! You guessed the number in "
                      << attempts << " attempts!\n";
-                cout << "Your guesses are: ";
 
-                for (int i = 0; i < attempts; i++) {
+                cout << "Your guesses are: ";
+                for (int i = 0; i < attempts; i++)
+                {
                     cout << guesses[i] << " ";
                 }
                 cout << endl;
@@ -48,10 +61,11 @@ int main() {
 
         cout << "\nDo you want to play again? (y/n): ";
         cin >> playAgain;
+        cin.ignore();
 
     } while (playAgain == 'y' || playAgain == 'Y');
 
-    cout << "\nThanks for playing!" << endl;
+    cout << "\nThanks for playing, " << playerName << "!" << endl;
 
     return 0;
 }
