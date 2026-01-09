cf914dc72073d0f7efe707e14009357103585e50
Gaurav <kalitagaurav47@gmail.com>
2026-01-10 01:19:16 +0530

    cb

--- Cows and bulls/cow and bull.cpp
+++ Cows and bulls/cow and bull.cpp
@@ -1,101 +1,125 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <unistd.h>
+#include <random>
+#include <string>
+#include <cctype>
 
 using namespace std;
 
-int main()
+int countCows(const string &userInput, const string &secretKey)
 {
-    srand(time(0));
-
-    int winning_number = rand() % 9000 + 1000;
-    int attempts = 0;
-
-    cout << "Welcome to the Cow and Bull number guessing game!\n"
-         << endl;
-
-    while (true)
+    int cows = 0;
+    for (int i = 0; i < 4; i++)
     {
-        int guess;
-        cout << "Guess the number: ";
-        cin >> guess;
-
-        if (guess < 1000 || guess > 9999)
+        if (userInput[i] == secretKey[i])
         {
-            cout << "Enter a valid 4-digit number.\n";
-            continue;
+            cows++;
         }
+    }
+    return cows;
+}
 
-        attempts++;
+int countBulls(const string &userInput, const string &secretKey)
+{
+    int bulls = 0;
+    bool used[4] = {false};
 
-        int winning_digits[4], guessDigits[4];
-        int tempWin = winning_number, tempGuess = guess;
-
-        for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++)
+    {
+        if (userInput[i] == secretKey[i])
         {
-            winning_digits[i] = tempWin % 10;
-            guessDigits[i] = tempGuess % 10;
-            tempWin /= 10;
-            tempGuess /= 10;
+            used[i] = true;
         }
+    }
 
-        int bulls = 0, cows = 0;
-        bool usedWin[4] = {false}, usedGuess[4] = {false};
-
-        for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++)
+    {
+        if (userInput[i] != secretKey[i])
         {
-            if (winning_digits[i] == guessDigits[i])
-            {
-                bulls++;
-                usedWin[i] = true;
-                usedGuess[i] = true;
-            }
-        }
-
-        for (int i = 0; i < 4; i++)
-        {
-            if (usedWin[i])
-                continue;
-
             for (int j = 0; j < 4; j++)
             {
-                if (usedGuess[j])
-                    continue;
-
-                if (winning_digits[i] == guessDigits[j])
+                if (!used[j] && userInput[i] == secretKey[j])
                 {
-                    cows++;
-                    usedWin[i] = true;
-                    usedGuess[j] = true;
+                    bulls++;
+                    used[j] = true;
                     break;
                 }
             }
         }
-
-        cout << "Cows: " << cows << ", Bulls: " << bulls << endl;
-
-        if (bulls == 4)
-        {
-            cout << "\nCongratulations! You guessed the number in "
-                 << attempts << " attempts.\n";
-
-            char playAgain;
-            cout << "Play again? (y/n): ";
-            cin >> playAgain;
-
-            if (playAgain == 'y' || playAgain == 'Y')
-            {
-                winning_number = rand() % 9000 + 1000;
-                attempts = 0;
-                cout << "\nNew game started!\n";
-            }
-            else
-                break;
-        }
-
-        sleep(1);
     }
 
+    return bulls;
+}
+
+int main()
+{
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1000, 9999);
+
+    string userInput;
+    char again;
+
+    cout << "------ Welcome to Cow & Bull Game ------\n\n";
+    cout << "RULES:\n";
+    cout << "1. Enter a 4-digit number\n";
+    cout << "2. COW  = correct digit & correct position\n";
+    cout << "3. BULL = correct digit & wrong position\n";
+    cout << "----------------------------------------\n\n";
+
+    do
+    {
+        int secretNumber = dis(gen);
+        string secretKey = to_string(secretNumber);
+
+        while (true)
+        {
+            cout << "Enter your guess: ";
+            cin >> userInput;
+
+            bool validInput = true;
+
+            if (userInput.length() != 4)
+                validInput = false;
+
+            for (char c : userInput)
+            {
+                if (!isdigit(c))
+                {
+                    validInput = false;
+                    break;
+                }
+            }
+
+            if (!validInput)
+            {
+                cout << "Invalid input! Enter a 4-digit number.\n\n";
+                continue;
+            }
+
+            int cows = countCows(userInput, secretKey);
+            int bulls = countBulls(userInput, secretKey);
+
+            cout << "Cows: " << cows << " , Bulls: " << bulls << endl;
+
+            if (cows == 4)
+            {
+                cout << "\nCOW WINS! Exact match!\n";
+                cout << "Secret number was: " << secretKey << endl;
+                break;
+            }
+            else if (bulls == 4)
+            {
+                cout << "\nBULL WINS! All digits correct but positions wrong!\n";
+                cout << "Secret number was: " << secretKey << endl;
+                break;
+            }
+        }
+
+        cout << "\nPlay again? (y/n): ";
+        cin >> again;
+
+    } while (again == 'y' || again == 'Y');
+
+    cout << "\nGame Over! Thanks for playing\n";
     return 0;
 }
